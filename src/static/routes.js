fd4720f0a5c462e863308990b04ea8acdf4b0822
Ilya Zhukov <izhukov1992@gmail.com>
2018-10-06 23:31:42 +0300

    Clean ui

--- src/static/routes.js
+++ src/static/routes.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
-import { HomeView, LoginView, ProtectedView, NotFoundView } from './containers';
-import requireAuthentication from './utils/requireAuthentication';
+import { HomeView, NotFoundView } from './containers';
 
 export default(
     <Switch>
         <Route exact path="/" component={HomeView} />
-        <Route path="/login" component={LoginView} />
-        <Route path="/protected" component={requireAuthentication(ProtectedView)} />
         <Route path="*" component={NotFoundView} />
     </Switch>
 
